e8bfd0325775a2044c2cb7ba9261c97e34885e37
petru.stepan <petru.stepan@D20-05>
2021-10-19 11:36:10 +0200

    hotovo

--- BMII/Program.cs
+++ BMII/Program.cs
@@ -6,7 +6,36 @@ namespace BMII
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double vyska;
+            double hmotnost;
+            double count;
+
+            Console.Clear();
+            Console.WriteLine(" výpočet BMI");
+            Console.WriteLine("--------------");
+            Console.ReadLine();
+
+            Console.WriteLine(" zadej hmotnost ");
+            double.TryParse(Console.ReadLine(), out hmotnost);
+            Console.ReadLine();
+
+            Console.WriteLine(" set vysku ");
+            double.TryParse(Console.ReadLine(), out vyska);
+            Console.ReadLine();
+
+            count = (hmotnost) / (vyska / 100)* (vyska / 100);
+            Console.WriteLine();
+            Console.WriteLine($"your BMI is {count} ");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
         }
     }
 }
